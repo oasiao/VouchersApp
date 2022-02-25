7438314a5e3e1df1b542bd2f4d5f13f31f7b3732
oasiao <74007996+oasiao@users.noreply.github.com>
2022-02-25 15:46:39 +0100

    Voucher seeder created for default values in table

---
+++ database/seeders/VoucherSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Http\VoucherHelper;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class VoucherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $helper = new VoucherHelper;
+        $vouchers = [
+            [
+                'id' => $helper->str_random(15),
+                'value' => 100,
+                'valid_date' => Carbon::parse('2022-03-25'),
+            ],
+            [
+                'id' => $helper->str_random(15),
+                'value' => 150,
+                'valid_date' => Carbon::parse('2022-04-25'),
+            ]
+        ];
+
+        DB::table('vouchers')->insert($vouchers);
+    }
+}
