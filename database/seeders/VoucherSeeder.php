13c8a66adaab0bb6d42489d8b09fd996ab4d6575
oasiao <74007996+oasiao@users.noreply.github.com>
2022-02-25 18:45:40 +0100

    Added more vouchers

--- database/seeders/VoucherSeeder.php
+++ database/seeders/VoucherSeeder.php
@@ -28,7 +28,37 @@ class VoucherSeeder extends Seeder
                 'id' => $helper->str_random(15),
                 'value' => 150,
                 'valid_date' => Carbon::parse('2022-04-25'),
-            ]
+            ],
+            [
+                'id' => $helper->str_random(15),
+                'value' => 130,
+                'valid_date' => Carbon::parse('2022-02-21'),
+            ],
+            [
+                'id' => $helper->str_random(15),
+                'value' => 20,
+                'valid_date' => Carbon::parse('2022-01-2'),
+            ],
+            [
+                'id' => $helper->str_random(15),
+                'value' => 10,
+                'valid_date' => Carbon::parse('2022-03-13'),
+            ],
+            [
+                'id' => $helper->str_random(15),
+                'value' => 300,
+                'valid_date' => Carbon::parse('2022-04-1'),
+            ],
+            [
+                'id' => $helper->str_random(15),
+                'value' => 100,
+                'valid_date' => Carbon::parse('2021-01-10'),
+            ],
+            [
+                'id' => $helper->str_random(15),
+                'value' => 80,
+                'valid_date' => Carbon::parse('2022-02-22'),
+            ],
         ];
 
         DB::table('vouchers')->insert($vouchers);
